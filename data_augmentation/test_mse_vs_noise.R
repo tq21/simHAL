f51c8ccd48d5e6999217ccc45d050597fc41f637
Sky Qiu <36117216+tq21@users.noreply.github.com>
2024-03-19 19:24:08 -0700

    update scripts

--- data_augmentation/test_mse_vs_noise.R
+++ data_augmentation/test_mse_vs_noise.R
@@ -15,17 +15,21 @@ d_vals <- c(1, 3, 5)
 max_degree <- 3
 scenarios <- c("smooth", "jump", "sin")
 noise_seq <- round(seq(0.01, 0.3, length.out = 20), 3)
-params <- expand.grid(d = d_vals, scenario = scenarios, noise = noise_seq, run = 1:B)
+params <- expand.grid(d = d_vals, scenario = scenarios, run = 1:B)
 params$scenario <- as.character(params$scenario)
-results_list <- list()
+results <- data.frame(run = NULL,
+                      d = NULL,
+                      scenario = NULL,
+                      noise = NULL,
+                      mse = NULL,
+                      num_selected = NULL)
 
 # RUN SIMULATION ---------------------------------------------------------------
 for (i in 1:nrow(params)) {
   param <- params[i, ]
   print("run: " %+% param$run %+%
           ", d: " %+% param$d %+%
-          ", scenario: " %+% param$scenario %+%
-          ", noise: " %+% param$noise)
+          ", scenario: " %+% param$scenario)
 
   # simulate data
   if (param$d == 1 & param$scenario == "smooth") {
@@ -81,37 +85,50 @@ for (i in 1:nrow(params)) {
   hal_pred <- predict(hal_fit, new_data = data_test[, 1:param$d, drop = FALSE])
   hal_mse <- get_loss(hal_pred, data_test[, param$d+1], "gaussian")
   hal_num_selected <- length(hal_fit$basis_list[hal_fit$coefs[-1] != 0])
+  res <- data.frame(run = param$run,
+                    d = param$d,
+                    scenario = param$scenario,
+                    noise = 0,
+                    mse = hal_mse,
+                    num_selected = hal_num_selected)
+  results <- rbind(results, res)
 
-  # fit augmented HAL
-  hal_aug_fit <- fit_hal_augment(X = data[, 1:param$d, drop = FALSE],
-                                 Y = data[, param$d+1],
-                                 col_idx = col_idx,
-                                 noise = param$noise,
-                                 copies = 5,
-                                 max_degree = max_degree,
-                                 basis_list = basis_list)$fit
-  hal_aug_pred <- predict(hal_aug_fit, new_data = data_test[, 1:param$d, drop = FALSE])
-  hal_aug_mse <- get_loss(hal_aug_pred, data_test[, param$d+1], "gaussian")
-  hal_aug_num_selected <- length(hal_aug_fit$basis_list[hal_aug_fit$coefs[-1] != 0])
+  for (noise in noise_seq) {
+    print("noise: " %+% noise)
 
-  # store results
-  results_list[[i]] <- list(run = param$run,
-                            d = param$d,
-                            scenario = param$scenario,
-                            noise = param$noise,
-                            hal_mse = hal_mse,
-                            hal_num_selected = hal_num_selected,
-                            hal_aug_mse = hal_aug_mse,
-                            hal_aug_num_selected = hal_aug_num_selected)
+    # fit augmented HAL
+    hal_aug_fit <- fit_hal_augment(X = data[, 1:param$d, drop = FALSE],
+                                   Y = data[, param$d+1],
+                                   col_idx = col_idx,
+                                   noise = noise,
+                                   copies = 5,
+                                   max_degree = max_degree,
+                                   basis_list = basis_list)$fit
+    hal_aug_pred <- predict(hal_aug_fit, new_data = data_test[, 1:param$d, drop = FALSE])
+    hal_aug_mse <- get_loss(hal_aug_pred, data_test[, param$d+1], "gaussian")
+    hal_aug_num_selected <- length(hal_aug_fit$basis_list[hal_aug_fit$coefs[-1] != 0])
+    res <- data.frame(run = param$run,
+                      d = param$d,
+                      scenario = param$scenario,
+                      noise = noise,
+                      mse = hal_aug_mse,
+                      num_selected = hal_aug_num_selected)
+    results <- rbind(results, res)
+  }
 }
 
-results <- do.call(rbind, lapply(results_list, data.frame, stringsAsFactors = FALSE))
 saveRDS(results, "out/test_mse_vs_noise.rds")
 
 # plot
-results_sum <- results %>%
-  summarize(avg_hal_mse = mean(hal_mse),
-            avg_hal_aug_mse = mean(hal_aug_mse),
+results_hal <- results %>%
+  filter(noise == 0) %>%
+  summarize(avg_mse = mean(mse),
+            avg_num_selected = mean(num_selected),
+            .by = c("d", "scenario"))
+results_aug <- results %>%
+  filter(noise != 0) %>%
+  summarize(avg_mse = mean(mse),
+            avg_num_selected = mean(num_selected),
             .by = c("d", "scenario", "noise"))
 plot_grid <- expand.grid(d = d_vals, scenario = scenarios)
 
@@ -120,13 +137,14 @@ par(mfrow = c(3, 3))
 for (i in 1:nrow(plot_grid)) {
   d_cur <- plot_grid[i, ]$d
   scenario_cur <- plot_grid[i, ]$scenario
-  tmp <- filter(results_sum, d == d_cur, scenario == scenario_cur)
-  plot(x = tmp$noise, y = tmp$avg_hal_aug_mse, type = "p", pch = 1,
-       ylim = c(min(tmp$avg_hal_mse, tmp$avg_hal_aug_mse),
-                max(tmp$avg_hal_mse, tmp$avg_hal_aug_mse)),
+  tmp_hal <- filter(results_hal, d == d_cur, scenario == scenario_cur)
+  tmp_aug <- filter(results_aug, d == d_cur, scenario == scenario_cur)
+  plot(x = tmp_aug$noise, y = tmp_aug$avg_mse, type = "p", pch = 1,
+       ylim = c(min(tmp_hal$avg_mse, tmp_aug$avg_mse),
+                max(tmp_hal$avg_mse, tmp_aug$avg_mse)),
        xlab = "Noise sd", ylab = "Test MSE",
        main = paste0("d = ", d_cur, ", ", scenario_cur))
-  lines(x = tmp$noise, y = tmp$avg_hal_aug_mse, type = "b")
-  abline(h = mean(tmp$avg_hal_mse), col = "red", lty = 2)
+  lines(x = tmp_aug$noise, y = tmp_aug$avg_mse, type = "b")
+  abline(h = mean(tmp_hal$avg_mse), col = "red", lty = 2)
 }
 dev.off()
