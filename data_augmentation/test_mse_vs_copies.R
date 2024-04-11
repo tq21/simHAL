b5f824cfcc6c4b1ba48ccc09c510543488ce6580
Sky Qiu <36117216+tq21@users.noreply.github.com>
2024-04-10 18:34:19 -0700

    Update test_mse_vs_copies.R

--- data_augmentation/test_mse_vs_copies.R
+++ data_augmentation/test_mse_vs_copies.R
@@ -5,16 +5,19 @@ library(dplyr)
 library(hal9001)
 library(devtools)
 library(purrr)
+library(origami)
 load_all()
 
 # SIMULATION PARAMETERS --------------------------------------------------------
-B <- 20
+B <- 10
 n <- 200
 n_test <- 10000
 d_vals <- c(1, 3, 5)
 max_degree <- 3
 scenarios <- c("smooth", "jump", "sin")
-copies_seq <- seq(1, 10)
+copies <- 5
+copies_max <- 10
+sd_seq <- round(seq(0.01, 0.3, length.out = 20), 3)
 params <- expand.grid(d = d_vals, scenario = scenarios, run = 1:B)
 params$scenario <- as.character(params$scenario)
 results <- data.frame(run = NULL,
@@ -36,47 +39,38 @@ for (i in 1:nrow(params)) {
     data <- sim_uni_smooth(n)
     data_test <- sim_uni_smooth(n_test)
     col_idx <- c(1)
-    noise <- 0.3
   } else if (param$d == 1 & param$scenario == "jump") {
     data <- sim_uni_jump(n)
     data_test <- sim_uni_jump(n_test)
     col_idx <- c(1)
-    noise <- 0.025
   } else if (param$d == 1 & param$scenario == "sin") {
     data <- sim_uni_sin(n)
     data_test <- sim_uni_sin(n_test)
     col_idx <- c(1)
-    noise <- 0.163
   } else if (param$d == 3 & param$scenario == "smooth") {
     data <- sim_tri_smooth(n)
     data_test <- sim_tri_smooth(n_test)
     col_idx <- c(1, 2)
-    noise <- 0.3
   } else if (param$d == 3 & param$scenario == "jump") {
     data <- sim_tri_jump(n)
     data_test <- sim_tri_jump(n_test)
     col_idx <- c(1, 2)
-    noise <- 0.041
   } else if (param$d == 3 & param$scenario == "sin") {
     data <- sim_tri_sin(n)
     data_test <- sim_tri_sin(n_test)
     col_idx <- c(1, 2)
-    noise <- 0.239
   } else if (param$d == 5 & param$scenario == "smooth") {
     data <- sim_five_smooth(n)
     data_test <- sim_five_smooth(n_test)
     col_idx <- c(1, 2, 4, 5)
-    noise <- 0.3
   } else if (param$d == 5 & param$scenario == "jump") {
     data <- sim_five_jump(n)
     data_test <- sim_five_jump(n_test)
     col_idx <- c(1, 2, 4, 5)
-    noise <- 0.041
   } else if (param$d == 5 & param$scenario == "sin") {
     data <- sim_five_sin(n)
     data_test <- sim_five_sin(n_test)
     col_idx <- c(1, 2, 4, 5)
-    noise <- 0.254
   }
 
   # make basis list
@@ -102,31 +96,35 @@ for (i in 1:nrow(params)) {
                     num_selected = hal_num_selected)
   results <- rbind(results, res)
 
-  for (copies in copies_seq) {
-    print("copies: " %+% copies)
-
-    # fit augmented HAL
-    hal_aug_fit <- fit_hal_augment(X = data[, 1:param$d, drop = FALSE],
-                                   Y = data[, param$d+1],
-                                   col_idx = col_idx,
-                                   noise = noise,
-                                   copies = copies,
-                                   max_degree = max_degree,
-                                   basis_list = basis_list)$fit
+  # fit augmented HAL
+  hal_aug_fit_obj_list <- fit_hal_augment(X = data[, 1:param$d, drop = FALSE],
+                                          Y = data[, param$d+1],
+                                          col_idx = col_idx,
+                                          sd_seq = sd_seq,
+                                          copies = copies,
+                                          family = "gaussian",
+                                          copies_max = copies_max,
+                                          max_degree = max_degree,
+                                          basis_list = basis_list)
+  res_aug <- map_dfr(hal_aug_fit_obj_list, function(.x) {
+    hal_aug_fit <- .x$fit
     hal_aug_pred <- predict(hal_aug_fit, new_data = data_test[, 1:param$d, drop = FALSE])
     hal_aug_mse <- get_loss(hal_aug_pred, data_test[, param$d+1], "gaussian")
     hal_aug_num_selected <- length(hal_aug_fit$basis_list[hal_aug_fit$coefs[-1] != 0])
     res <- data.frame(run = param$run,
                       d = param$d,
                       scenario = param$scenario,
-                      copies = copies,
+                      copies = .x$copies,
                       mse = hal_aug_mse,
                       num_selected = hal_aug_num_selected)
-    results <- rbind(results, res)
-  }
+
+    return(res)
+  })
+
+  results <- rbind(results, res_aug)
 }
 
-saveRDS(results, "out/test_mse_vs_copies.rds")
+saveRDS(results, "out/test_mse_vs_copies_cv.rds")
 
 # plot
 results_hal <- results %>%
