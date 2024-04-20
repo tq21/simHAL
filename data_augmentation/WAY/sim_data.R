5ba718a56a27026e155fb259c4a4cd14b7f57136
Sky Qiu <36117216+tq21@users.noreply.github.com>
2024-04-19 22:51:13 -0700

    typo

--- data_augmentation/WAY/sim_data.R
+++ data_augmentation/WAY/sim_data.R
@@ -15,8 +15,8 @@ sim_data <- function(n, data_id) {
     Y <- 1.5*A+W1/15-0.28*W1^2+0.5*W2+0.25*W2*W3+U
   } else if (data_id == 2) {
     Y <- 1.5*A-2*as.numeric(W1 < -3)*W3+2.5*as.numeric(W1 > -2)-
-      2*as.numeric(W1 > 0)+2.5*as.numeric(W1 > 2)*X3-2.5*as.numeric(W1 > 3) +
-      as.numeric(W2 > -1)-4*as.numeric(W2 > 1)*X3+2*as.numeric(W2 > 3)+U
+      2*as.numeric(W1 > 0)+2.5*as.numeric(W1 > 2)*W3-2.5*as.numeric(W1 > 3) +
+      as.numeric(W2 > -1)-4*as.numeric(W2 > 1)*W3+2*as.numeric(W2 > 3)+U
   } else if (data_id == 3) {
     Y <- 1.5*A+W3*(4*sin(pi/2*abs(W1))*(W2 < 0)+4.1*cos(pi/2*abs(W1))*(W2 > 0))+U
   }
